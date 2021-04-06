e9249521b6f58476b81ad72c9c4de0e7e51248a3
Niccolò Scatena <speedjack95@gmail.com>
2021-04-06 17:43:41 +0200

    Add argument to change controller ip addr to scripts

--- scripts/start_mininet_simple.sh
+++ scripts/start_mininet_simple.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+if [ $# -eq 0 ]
+then
+	CONTROLLERIP=127.0.0.1
+else
+	CONTROLLERIP="$1"
+fi
+
 sudo mn --topo single,3 \
 --mac \
 --switch ovsk \
---controller remote,ip=127.0.0.1,port=6653,protocols=OpenFlow13 \
+--controller remote,ip=$CONTROLLERIP,port=6653,protocols=OpenFlow13 \
 --ipbase=10.0.0.0
--- scripts/start_mininet_spine_esempio.sh
+++ scripts/start_mininet_spine_esempio.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo mn --custom spine-leaf.py --topo tower --mac --switch ovsk --controller remote,ip=127.0.0.1,port=6653,protocols=OpenFlow13
+if [ $# -eq 0 ]
+then
+	CONTROLLERIP=127.0.0.1
+else
+	CONTROLLERIP="$1"
+fi
+sudo mn --custom spine-leaf.py --topo tower --mac --switch ovsk --controller remote,ip=$CONTROLLERIP,port=6653,protocols=OpenFlow13
